4d2b3f2d09c2e2944be3430cec2855230467d947
Samiran_Das <samiran4209@gmail.com>
2024-06-01 22:46:58 +0530

    loots of stuf

--- backend/src/controllers/address.controller.js
+++ backend/src/controllers/address.controller.js
@@ -4,5 +4,22 @@ export const Address =async(req,res)=>{
     const {username,state,city,zipcode,locatioon}=req.body
 
 
+if (!(username && state && city && zipcode && locatioon)) {
+    return res.status(400).json("have to fill all the fields")
+}
+const address=await Address.create({
+    username,city,zipcode,locatioon
+})
 
+
+if (!address) {
+    return res.status(401).json(" failed to create address")
+}
+
+return res.status(200).json("user address",address)
+
+}
+
+export const updateAddress=async(req,res)=>{
+    const {username}=req.params
 }
