f9da27ff509d2903d04082870947efee7c21d92d
Samiran_Das <samiran4209@gmail.com>
2024-08-28 23:02:43 +0530

    ""

--- frontend/daru-frontend/src/App.jsx
+++ frontend/daru-frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 
 import Navbar from './components/navbar/Navbar'
-import Home from './components/homePage/Home'
-import Category from './components/Category/Category'
-import Overview from './components/Overview/Overview'
+
 import { Route, Routes } from 'react-router-dom'
-import CustomRoutes from './components/routes/CustomRoutes'
+import CustomRoutes from './components/routes/Routes'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,9 +18,7 @@ function App() {
 
      
     </Routes>
-    {/* <Home/>
-    <Category/>
-    <Overview/> */}
+    
     </>
   )
 }
